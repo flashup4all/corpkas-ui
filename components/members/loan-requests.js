8f498f9808e3bdc3b9819060c31ffcc4d65df2f7
Bardeson Lucky <flashup4all@gmail.com>
2020-09-14 21:35:52 +0100

    updates

--- components/members/loan-requests.js
+++ components/members/loan-requests.js
@@ -8,6 +8,7 @@ import EmptyData from '../../layouts/empty';
 import { getUser } from '../../components/shared/local';
 import swal from '@sweetalert/with-react'
 import CrossCircleIcon from '@atlaskit/icon/glyph/cross-circle';
+import { Checkbox } from '@atlaskit/checkbox';
 
 const LOAN_TYPES = gql`
 query {
@@ -58,6 +59,7 @@ const LoanRequests = ({memberData}) =>  {
 
     const { addToast } = useToasts()
     const [memberLoans, setMemberLoans] = useState([])
+    const [firstLoan, setMemberFirstLoan] = useState([])
     const [sorted, setSorted] = useState([])
     const [setMode, setPageMode] = useState(0)
     const [loanTypes, setLoanTypes] = useState([])
@@ -97,6 +99,7 @@ const LoanRequests = ({memberData}) =>  {
         onCompleted: ({memberLoans}) =>{
             console.log(memberLoans)
             setMemberLoans(memberLoans)
+            setMemberFirstLoan(memberLoans[0])
             setSorted(memberLoans)
         }
     })
@@ -111,14 +114,21 @@ const LoanRequests = ({memberData}) =>  {
             },
             
         })
+        console.log(firstLoan)
     const [loan_type_id, setLoanTypeId] = useState()
-    const [loan_amount, setLoanAmount] = useState()
+    const [loan_amount, setLoanAmount] = useState(firstLoan.loan_amount)
     const [monthly_deduction, setMonthlyDeduction] = useState()
-    const [reason, setReason] = useState()
-
-    // update loan state
-    const [duration, setDuration] = useState()
-    // const [reason, setReason] = useState()
+    const [total_deduction, setTotalDeduction] = useState()
+    const [total_loan, setTotalLoan] = useState()
+    const [total_paid, setTotalPaid] = useState()
+    const [upfront_deduction, setUpfrontDeduction] = useState(firstLoan.upfront_deduction)
+    const [upfront_deduction_charge, setUpfrontDeductionCharge] = useState()
+    const [is_insured, setIsInsured] = useState(firstLoan.is_insured)
+    const [insurance_charge, setInsurance] = useState()
+    const [amount_payable, setAmountPayable] = useState()
+    const [actual_amount, setActualAmount] = useState(firstLoan.actual_amount)
+    const [duration, setDuration] = useState(firstLoan.duration)
+    const [reason, setReason] = useState(firstLoan.reason)
 
     // const [account_no, setAccountNo] = useState()
    
@@ -165,16 +175,17 @@ const LoanRequests = ({memberData}) =>  {
                 { setMode == 0 && memberLoans.length > 0 && 
                 <div>
                     {memberLoans.length > 0 && 
-                        memberLoans.map(loan => {
-                            return(
-                            <form key={loan.id} onSubmit={submit}>
+                        // setIsInsured(loan.is_insured)
+                            // setUpfrontDeduction(loan.upfront_deduction)
+
+                            <form onSubmit={submit}>
                                 <div className="row mt-5 pb-5 bg-white">
                                     <div className="col-md-6 mt-3">
-                                        <p className="ks-request-text">Loan request for ₦ {loan.actual_amount}</p>
+                                        <p className="ks-request-text">Loan request for ₦ {firstLoan.actual_amount}</p>
                                         <p className="">Loan Requests</p>
                                     </div>
                                     <div className="col-md-6 mt-3">
-                                    <p className="ks-request-text float-right">Loan request for ₦ {loan.actual_amount}</p>
+                                    <p className="ks-request-text float-right">Loan request for ₦ {firstLoan.actual_amount}</p>
                                     <p className="float-right">Loan Requests</p>
                                     </div>
                                 <div className="loan-request-row">
@@ -182,7 +193,7 @@ const LoanRequests = ({memberData}) =>  {
                                     <div className="col-md-3">
                                         <label className="ks-label">Type of Loan</label>
                                         <select className="ks-form-control form-control" 
-                                            value={loan.loan_type_id || ""}
+                                            value={loan_type_id || ""}
                                             onChange={({ target }) => setLoanTypeId(target.value)}
                                             >
                                             <option value="">Options</option>
@@ -194,7 +205,7 @@ const LoanRequests = ({memberData}) =>  {
                                         <label className="ks-label">Period of Repayment</label>
                                         <input className="ks-form-control form-control" 
                                             placeholder="18 months"
-                                            value={loan.duration || ""}
+                                            value={duration || ""}
                                             onChange={({ target }) => setDuration(target.value)}
                                         />
                                     </div>
@@ -202,7 +213,7 @@ const LoanRequests = ({memberData}) =>  {
                                         <label className="ks-label">Loan Amount</label>
                                         <input className="ks-form-control form-control"
                                         placeholder="Access Diamond"
-                                            value={loan.loan_amount || ""}
+                                            value={loan_amount || ""}
                                             onChange={({ target }) => setLoanAmount(target.value)}
                                         />
                                     </div>
@@ -210,7 +221,7 @@ const LoanRequests = ({memberData}) =>  {
                                         <label className="ks-label">Monthly Deduction</label>
                                         <input className="ks-form-control form-control"
                                         placeholder="Access Diamond"
-                                            value={loan.monthly_deduction || ""}
+                                            value={monthly_deduction || ""}
                                             onChange={({ target }) => setLoanAmount(target.value)}
                                         />
                                     </div>
@@ -223,32 +234,92 @@ const LoanRequests = ({memberData}) =>  {
                                         />
                                     </div> */}
                                 
-                                
-                                </div>
-                                <div className="row d-flex justify-content-center">
-                                    <div className="col-md-4">
-                                        <img src="/images/loan.svg" className="img-responsive" alt="Some picture" width="410" height="307"></img>
-
+                                    <div className="row d-flex justify-content-center">
+                                        <div className="col-md-4">
+                                            <img src="/images/loan.svg" className="img-responsive" alt="Some picture" width="410" height="307"></img>
+                                            </div>
+                                        <div className="col-md-4">
+                                            <p className="text mt-5">We are currently working on your loan and we will get back to you soon with an offer. If you think this is taking longer than it should, feel free to leave us a follow up messgae.</p>
                                         </div>
-                                    <div className="col-md-4">
-                                        <p className="text mt-5">We are currently working on your loan and we will get back to you soon with an offer. If you think this is taking longer than it should, feel free to leave us a follow up messgae.</p>
                                     </div>
-                                </div>
-                                <div className="row d-flex justify-content-center">  
-                                
-                                </div>
-                                <div className="col-12">
+                                    <div className="row col-md-12">
+                                        <div className="col-md-3">
+                                            <Checkbox
+                                                isChecked={is_insured}
+                                                label="Apply Insurance"
+                                                value={is_insured}
+                                                onChange={(e) => setIsInsured(e.target.checked)}
+                                                name="checkbox-default"
+                                                testId="cb-default"
+                                            />
+                                        </div>
+                                        <div className="col-md-3">
+                                            <Checkbox
+                                                isChecked={upfront_deduction}
+                                                value={upfront_deduction}
+                                                label="Upfront Deduction"
+                                                onChange={(e) => setUpfrontDeduction(e.target.checked)}
+                                                name="checkbox-default"
+                                                testId="cb-default"
+                                            />
+                                        </div>
+                                    </div>
+                                    
+                                    {
+                                        is_insured && 
+                                        <div className="col-md-3">
+                                            <label className="ks-label">Insurance Charge</label>
+                                            <input className="ks-form-control form-control"
+                                            placeholder="Access Diamond"
+                                                value={insurance_charge || ""}
+                                                onChange={({ target }) => setInsurance(target.value)}
+                                            />
+                                        </div>
+                                    }
+                                    {
+                                        upfront_deduction && 
+                                        <div className="col-md-3">
+                                            <label className="ks-label">Upfront Deduction</label>
+                                            <input className="ks-form-control form-control"
+                                            placeholder="Access Diamond"
+                                                value={upfront_deduction_charge || ""}
+                                                onChange={({ target }) => setUpfrontDeductionCharge(target.value)}
+                                            />
+                                        </div>
+                                    }
+                                    
+                                    {/* <div className="col-md-3">
+                                        <label className="ks-label">Monthly Deduction</label>
+                                        <input className="ks-form-control form-control"
+                                        placeholder="Access Diamond"
+                                            value={loan.monthly_deduction || ""}
+                                            onChange={({ target }) => setLoanAmount(target.value)}
+                                        />
+                                    </div> */}
+                                    <div className="col-12">
+                                    <button className="btn float-left mt-5 btn-danger " type="submit">
+                                        {/* disabled={loading} */}
+                                        {/* {
+                                            loading &&
+                                            <Spinner appearance="invert" size="medium"/>
+                                        } */}
+                                        Cancel</button>
                                         <button className="btn float-right mt-5 " type="submit">
                                         {/* disabled={loading} */}
                                         {/* {
                                             loading &&
                                             <Spinner appearance="invert" size="medium"/>
                                         } */}
-                                        APPLY</button>
+                                        APPjLY</button>
                                     </div>
+                                </div>
+                                
+                                <div className="row d-flex justify-content-center">  
+                                
+                                </div>
+                                
                             </form>
-                            )
-                        })
+                        
                     }
                 </div>
                     
