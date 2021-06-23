ff51f321166e85b53f958a2212f726be0ba4c2d4
Benjamin Guma <benjaminguma.bg@gmail.com>
2021-06-23 06:13:34 +0100

    dropdown and route tweak

--- layouts/sidebar/sidebar-nav.js
+++ layouts/sidebar/sidebar-nav.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react"
-import Link from 'next/link'
-import Router  from 'next/router';
+import React, {useEffect} from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
 
 import HomeIcon from '@atlaskit/icon/glyph/home';
 import SettingsIcon from '@atlaskit/icon/glyph/settings';
@@ -11,57 +11,61 @@ import DetailViewIcon from '@atlaskit/icon/glyph/detail-view';
 import EditIcon from '@atlaskit/icon/glyph/edit';
 import UnlockIcon from '@atlaskit/icon/glyph/unlock';
 import TrayIcon from '@atlaskit/icon/glyph/tray';
-import Dropdown from 'react-bootstrap/Dropdown'
-import {clearStorage, getVendor, getUser, getStaff, getMember} from '../../components/shared/local'
-import swal from '@sweetalert/with-react'
+import Dropdown from 'react-bootstrap/Dropdown';
+import {
+  clearStorage,
+  getVendor,
+  getUser,
+  getStaff,
+  getMember,
+} from '../../components/shared/local';
+import swal from '@sweetalert/with-react';
 
 class SideBarNav extends React.Component {
-
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super (props);
 
     this.state = {
       user: '',
       staff: '',
       member: '',
-      activeMenuIndex: 0
-    }
+      activeMenuIndex: 0,
+    };
   }
 
-  componentDidMount(){
-    let user = getUser()
-    let staff = getStaff()
-    let member = getMember()
-    this.setState({
+  componentDidMount () {
+    let user = getUser ();
+    let staff = getStaff ();
+    let member = getMember ();
+    this.setState ({
       user: user,
       staff: staff,
       member: member,
-    })
+    });
   }
 
-  logout(){
-    swal({
-      title: "Are you sure?",
-      text: "You want to Logout!",
-      icon: "warning",
+  logout () {
+    swal ({
+      title: 'Are you sure?',
+      text: 'You want to Logout!',
+      icon: 'warning',
       buttons: true,
       dangerMode: true,
-    })
-    .then((yes) => {
-      clearStorage()
-      Router.push('/')
-    })
+    }).then (yes => {
+      clearStorage ();
+      Router.push ('/');
+    });
   }
-  render()
-  {
-    const { user, member, staff, activeMenuIndex } = this.state
-    const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+
+  render () {
+    const {user, member, staff, activeMenuIndex} = this.state;
+    const CustomToggle = React.forwardRef (({children, onClick}, ref) => (
       <a
         href=""
         ref={ref}
-        onClick={(e) => {
-          e.preventDefault();
-          onClick(e);
+        onClick={e => {
+          e.preventDefault ();
+          onClick (e);
         }}
       >
         {children}
@@ -69,55 +73,134 @@ class SideBarNav extends React.Component {
       </a>
     ));
 
-    
-    
-    return ( 
+    return (
       <div className="sidebar-navigation" id="sidebar-wrapper">
-                {/* <div className="sidebar-heading">Start Bootstrap </div> */}
-            { user.role !== "member" && 
-            <div className="sidebar-navigation">
-                <ul>
-                <li className={activeMenuIndex === 0 ? 'active-side-menu' : 'link'} onClick={() => this.setState({activeMenuIndex: 0})}><Link href="/dashboard"><a><HomeIcon /> <span className="ml-2">Dashboard</span></a></Link></li>
-                <li className={activeMenuIndex === 1 ? 'active-side-menu' : 'link'} onClick={() => this.setState({activeMenuIndex: 1})}><Link href="/members"><a><PeopleIcon />  <span className="ml-2">Manage Members</span></a></Link></li>
-                <li className={activeMenuIndex === 2 ? 'active-side-menu' : 'link'} onClick={() => this.setState({activeMenuIndex: 2})}><Link href="/transactions"><a><CreditcardIcon /> <span className="ml-2">manage transactions</span></a></Link></li>
-                <li className={activeMenuIndex === 3 ? 'active-side-menu' : 'link'} onClick={() => this.setState({activeMenuIndex: 3})}><Link href="/loans"><a><TrayIcon />  <span className="ml-2">Manage Loans</span></a></Link></li>
-                <li className={activeMenuIndex === 4 ? 'active-side-menu' : 'link'} onClick={() => this.setState({activeMenuIndex: 4})}><Link href="/vault"><a><CreditcardIcon />  <span className="ml-2">Manage Vault</span></a></Link></li>
-                <li className={activeMenuIndex === 5 ? 'active-side-menu' : 'link'} onClick={() => this.setState({activeMenuIndex: 5})}>
+        {/* <div className="sidebar-heading">Start Bootstrap </div> */}
+        {user.role !== 'member' &&
+          <div className="sidebar-navigation">
+            <ul>
+              <li
+                className={activeMenuIndex === 0 ? 'active-side-menu' : 'link'}
+                onClick={() => this.setState ({activeMenuIndex: 0})}
+              >
+                <Link href="/dashboard">
+                  <a><HomeIcon /> <span className="ml-2">Dashboard</span></a>
+                </Link>
+              </li>
+              <li
+                className={activeMenuIndex === 1 ? 'active-side-menu' : 'link'}
+                onClick={() => this.setState ({activeMenuIndex: 1})}
+              >
+                <Link href="/members">
+                  <a>
+                    <PeopleIcon />  <span className="ml-2">Manage Members</span>
+                  </a>
+                </Link>
+              </li>
+              <li
+                className={activeMenuIndex === 2 ? 'active-side-menu' : 'link'}
+                onClick={() => this.setState ({activeMenuIndex: 2})}
+              >
+                <Link href="/transactions">
+                  <a>
+                    <CreditcardIcon />
+                    {' '}
+                    <span className="ml-2">manage transactions</span>
+                  </a>
+                </Link>
+              </li>
+              <li
+                className={activeMenuIndex === 3 ? 'active-side-menu' : 'link'}
+                onClick={() => this.setState ({activeMenuIndex: 3})}
+              >
+                <Link href="/loans">
+                  <a>
+                    <TrayIcon />  <span className="ml-2">Manage Loans</span>
+                  </a>
+                </Link>
+              </li>
+              <li
+                className={activeMenuIndex === 4 ? 'active-side-menu' : 'link'}
+                onClick={() => this.setState ({activeMenuIndex: 4})}
+              >
+                <Link href="/vault">
+                  <a>
+                    <CreditcardIcon />
+                    {' '}{' '}
+                    <span className="ml-2">Manage Vault</span>
+                  </a>
+                </Link>
+              </li>
+              <li
+                className={activeMenuIndex === 5 ? 'active-side-menu' : 'link'}
+                id="x"
+                onClick={() => this.setState ({activeMenuIndex: 5})}
+              >
                 <Dropdown className="link settings-side-menu">
-                  <Dropdown.Toggle as={CustomToggle} id="dropdown-basic">
-                  <SettingsIcon /> <span className="ml-2 mr-2">Settings</span>
+                  <Dropdown.Toggle id="dropdown-basic">
+                    <SettingsIcon /> <span className="ml-2 mr-2">Settings</span>
                   </Dropdown.Toggle>
                   <Dropdown.Menu className="ks-menu-dropdown bg-menu">
-                    <Dropdown.Item className="ks-menu-dropdown-item" onClick={() => Router.replace('vendor-profile')}>Cooperative Profile</Dropdown.Item>
+                    <Dropdown.Item
+                      className="ks-menu-dropdown-item"
+                      onClick={() => Router.push ('vendor-profile')}
+                    >
+                      Cooperative Profile
+                    </Dropdown.Item>
                     <Dropdown.Divider />
-                    <Dropdown.Item className="ks-menu-dropdown-item" onClick={() => Router.replace('staff')}>Staff Settings</Dropdown.Item>
+                    <Dropdown.Item
+                      className="ks-menu-dropdown-item"
+                      onClick={() => Router.push ('staff')}
+                    >
+                      Staff Settings
+                    </Dropdown.Item>
                     <Dropdown.Divider />
-                    <Dropdown.Item className="ks-menu-dropdown-item" onClick={() => Router.replace('/loans/settings')}>Loan Settings</Dropdown.Item>
-                    <Dropdown.Divider />
+                    <Dropdown.Item
+                      className="ks-menu-dropdown-item"
+                      onClick={() => Router.push ('/loans/settings')}
+                    >
+                      Loan Settings
+                    </Dropdown.Item>
+
                   </Dropdown.Menu>
                 </Dropdown>
-                </li>
-                        
-                </ul>
-            </div>
-            }    
-        { user.role == "member" && 
-            <div className="sidebar-navigation">
-                <ul>
-                <li className={activeMenuIndex === 0 ? 'active-side-menu' : 'link'}><Link href="/members-app"><a><HomeIcon /> <span className="ml-2">Dashboard</span></a></Link></li>
-                {/* <li className='link'><Link href="/loans"><a><TrayIcon />  <span className="ml-2">Manage Loans</span></a></Link></li> */}
-                <li className={activeMenuIndex === 1 ? 'active-side-menu' : 'link'}><Link href="/update-password"><a><EditIcon />  <span className="ml-2">Update Password</span></a></Link></li>
-                <li className={activeMenuIndex === 2 ? 'active-side-menu' : 'link'} onClick={() => this.logout()}><a><UnlockIcon />  <span className="ml-2">Logout</span></a></li>
-                        
-                </ul>
-            </div>
-            }    
+              </li>
 
-        </div>
-  
-        )
+            </ul>
+          </div>}
+        {user.role == 'member' &&
+          <div className="sidebar-navigation">
+            <ul>
+              <li
+                className={activeMenuIndex === 0 ? 'active-side-menu' : 'link'}
+              >
+                <Link href="/members-app">
+                  <a><HomeIcon /> <span className="ml-2">Dashboard</span></a>
+                </Link>
+              </li>
+              {/* <li className='link'><Link href="/loans"><a><TrayIcon />  <span className="ml-2">Manage Loans</span></a></Link></li> */}
+              <li
+                className={activeMenuIndex === 1 ? 'active-side-menu' : 'link'}
+              >
+                <Link href="/update-password">
+                  <a>
+                    <EditIcon />  <span className="ml-2">Update Password</span>
+                  </a>
+                </Link>
+              </li>
+              <li
+                className={activeMenuIndex === 2 ? 'active-side-menu' : 'link'}
+                onClick={() => this.logout ()}
+              >
+                <a><UnlockIcon />  <span className="ml-2">Logout</span></a>
+              </li>
+
+            </ul>
+          </div>}
+
+      </div>
+    );
   }
-    
-  }
+}
 
 export default SideBarNav;
--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,56 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import { useMutation } from '@apollo/client';
-import {useRouter}  from 'next/router';
+import React, {useState, useEffect} from 'react';
+import {useMutation} from '@apollo/client';
+import {useRouter} from 'next/router';
 import Spinner from '@atlaskit/spinner';
-import { LOGIN } from '../../gql/user'
+import {LOGIN} from '../../gql/user';
 
-const LoginForm = ({ setError, setToken }) =>{
-    const router = useRouter()
+const LoginForm = ({setError, setToken}) => {
+  const router = useRouter ();
 
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
+  const [email, setEmail] = useState ();
+  const [password, setPassword] = useState ();
 
-  const [ authenticate, { loading, error } ] = useMutation(LOGIN, {
-    onError: (error) => {
-        console.log(error)
-    //   setError(error.graphQLErrors[0].message)
+  const [authenticate, {loading, error}] = useMutation (LOGIN, {
+    onError: error => {
+      console.log (error);
+      //   setError(error.graphQLErrors[0].message)
     },
-    onCompleted({authenticate}){
-      const token = authenticate.token
-      const user = authenticate.user
-      localStorage.setItem('katk', token)
-      localStorage.setItem('kutk', JSON.stringify(user))
-      if(user.role === "member")
-      {
-          const member = authenticate.member
-          localStorage.setItem('kmtk', JSON.stringify(member))
+    onCompleted({authenticate}) {
+      const token = authenticate.token;
+      const user = authenticate.user;
+      localStorage.setItem ('katk', token);
+      localStorage.setItem ('kutk', JSON.stringify (user));
+      if (user.role === 'member') {
+        const member = authenticate.member;
+        localStorage.setItem ('kmtk', JSON.stringify (member));
       }
-      if(user.role === "admin" || user.role === "staff" || user.role === "manager")
-      {
-          const staff = authenticate.staff
-          localStorage.setItem('kstk', JSON.stringify(staff))
+      if (
+        user.role === 'admin' ||
+        user.role === 'staff' ||
+        user.role === 'manager'
+      ) {
+        const staff = authenticate.staff;
+        localStorage.setItem ('kstk', JSON.stringify (staff));
       }
       // setTimeout(() => {
-          router.push('/dashboard')
+      window.location.replace ('/dashboard');
       // }, 200);
-    }
-
-  })
+    },
+  });
 
   // useEffect(() => {
   //   if ( result.data ) {
   //       console.log(result)
-  //     
-      
-  //     //   setToken(token)
-  //       
-  //   }
-  // }, [result.data]) 
+  //
 
-  const submit = async (event) => {
-    event.preventDefault()
-    authenticate({ variables: { email, password } })
-  }
+  //     //   setToken(token)
+  //
+  //   }
+  // }, [result.data])
+
+  const submit = async event => {
+    event.preventDefault ();
+    authenticate ({variables: {email, password}});
+  };
 
   return (
     <div className="container">
@@ -60,42 +61,41 @@ const LoginForm = ({ setError, setToken }) =>{
             <div className="card-body">
               <h5 className="card-title text-center">Sign In</h5>
               <form className="form-signin" onSubmit={submit}>
-                  <div className="form-label-group">
-                    <label htmlFor="inputEmail">Email</label>
-                    <input
-                      className="form-control"
-                      type="email"
-                      value={email || ""}
-                      onChange={({ target }) => setEmail(target.value)}
-                    />
-                    
-                  </div>
-                  <div>
-                    <label htmlFor="inputEmail">Password</label>
-                    <input
-                      className="form-control"
-                      type='password'
-                      value={password || ""}
-                      onChange={({ target }) => setPassword(target.value)}
-                    />
-    
-                  </div>
-                  <button disabled={loading} className="btn btn-md btn-primary btn-block text-uppercase" type='submit'>
-                      {
-                        loading &&
-                        <Spinner appearance="invert" size="medium"/>
-                      }
-                     <span className="ml-2" >login</span>
-                     </button>
+                <div className="form-label-group">
+                  <label htmlFor="inputEmail">Email</label>
+                  <input
+                    className="form-control"
+                    type="email"
+                    value={email || ''}
+                    onChange={({target}) => setEmail (target.value)}
+                  />
+
+                </div>
+                <div>
+                  <label htmlFor="inputEmail">Password</label>
+                  <input
+                    className="form-control"
+                    type="password"
+                    value={password || ''}
+                    onChange={({target}) => setPassword (target.value)}
+                  />
+
+                </div>
+                <button
+                  disabled={loading}
+                  className="btn btn-md btn-primary btn-block text-uppercase"
+                  type="submit"
+                >
+                  {loading && <Spinner appearance="invert" size="medium" />}
+                  <span className="ml-2">login</span>
+                </button>
               </form>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default LoginForm;
-
-
